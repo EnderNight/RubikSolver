2b62be941e9c97beb9b8161819b36c54830ebc2f
Ender <ndxendernight@gmail.com>
2024-03-31 17:08:31 +0200

    Added reading files

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,22 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
-int main()
+void print_usage() { std::cout << "Usage: solver [cube.esl]" << std::endl; }
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!" << std::endl;
+    if (argc != 2) {
+        print_usage();
+        return 0;
+    }
+
+    std::ifstream file(argv[1]);
+    std::stringstream buffer;
+
+    buffer << file.rdbuf();
+
+    std::cout << buffer.str() << std::endl;
 
     return 0;
 }
